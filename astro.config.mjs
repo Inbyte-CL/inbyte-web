13f14b373cb55ce621543ffbbfc1f869d7895743
Francisco Morales <francisco.morales>
2025-11-27 11:09:33 -0500

    Web v1

---
+++ astro.config.mjs
@@ -0,0 +1,66 @@
+import { defineConfig } from "astro/config";
+
+import tailwindcss from "@tailwindcss/vite";
+import sitemap from "@astrojs/sitemap";
+import mdx from "@astrojs/mdx";
+import AutoImport from "astro-auto-import";
+import react from "@astrojs/react";
+import keystatic from "@keystatic/astro";
+import netlify from "@astrojs/netlify";
+import compress from "@playform/compress";
+import icon from "astro-icon";
+
+// https://astro.build/config
+export default defineConfig({
+	site: "https://zenith.cosmicthemes.com",
+	adapter: netlify({
+		imageCDN: false,
+	}),
+	redirects: {
+		"/admin": "/keystatic",
+	},
+	// i18n configuration must match src/config/translations.json.ts
+	i18n: {
+		defaultLocale: "en",
+		locales: ["en", "fr"],
+		routing: {
+			prefixDefaultLocale: false,
+		},
+	},
+	markdown: {
+		shikiConfig: {
+			// Shiki Themes: https://shiki.style/themes
+			theme: "css-variables",
+			wrap: true,
+		},
+	},
+	integrations: [
+		// example auto import component into blog post mdx files
+		AutoImport({
+			imports: [
+				// https://github.com/delucis/astro-auto-import
+				"@/components/admonition/Admonition.astro",
+			],
+		}),
+		mdx(),
+		react(),
+		icon(),
+		keystatic(),
+		sitemap(),
+		compress({
+			HTML: true,
+			JavaScript: true,
+			CSS: false, // enabling this can cause issues
+			Image: false, // astro:assets handles this. Enabling this can dramatically increase build times
+			SVG: false,
+		}),
+	],
+
+	vite: {
+		plugins: [tailwindcss()],
+		// fix for client router script duplication issues on 404 page
+		build: {
+			assetsInlineLimit: 0,
+		},
+	},
+});
